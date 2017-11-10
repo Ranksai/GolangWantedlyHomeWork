0293ce8188736ccd136e63f92073d3640554d416
ranks <saito@room.rat.cis.k.hosei.ac.jp>
2017-11-10 20:57:01 +0900

    "user テーブルの構造体を追加しました。"

---
+++ src/model/row/user.go
@@ -0,0 +1,15 @@
+package row
+
+import (
+	"time"
+)
+
+type User struct {
+	Id        int       `json:"id" xorm:"'id'"`
+	Name      string    `json:"name" xorm:"'name'"`
+	Email     string    `json:"email" xorm:"'email'"`
+	CreatedAt time.Time `json:"created_at" xorm:"'created_at'"`
+	UpdatedAr time.Time `json:"updated_ar" xorm:"'updated_at'"`
+}
+
+type Users []User
