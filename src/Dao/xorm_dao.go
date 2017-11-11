040feb5def42641080090a35decf9144fd4466ec
ranks <saito@room.rat.cis.k.hosei.ac.jp>
2017-11-11 20:16:01 +0900

    "環境変数"

--- src/Dao/xorm_dao.go
+++ src/Dao/xorm_dao.go
@@ -1,14 +1,42 @@
 package Dao
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/go-xorm/xorm"
 	"github.com/labstack/gommon/log"
 	_ "github.com/lib/pq"
 )
 
 func NewXormHandler() *xorm.Engine {
-	url := "user=postgres host=localhost port=5432 dbname=wantedlyhomework sslmode=disable"
-	engine, err := xorm.NewEngine("postgres", url)
+	host := os.Getenv("POSTGRES_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "5432"
+	}
+	_, err := strconv.Atoi(port)
+	if err != nil {
+		log.Error(err)
+	}
+	user := os.Getenv("POSTGRES_USER")
+	if user == "" {
+		user = "postgres"
+	}
+	dbname := "wantedlyhomework"
+
+	dsn := fmt.Sprintf(
+		"user=%s host=%s port=%s dbname=%s sslmode=disable",
+		user,
+		host,
+		port,
+		dbname,
+	)
+	engine, err := xorm.NewEngine("postgres", dsn)
 	if err != nil {
 		log.Error(err)
 		return nil
