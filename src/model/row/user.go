96932dd0e15f356856c7ff7a5e70b2ac618c117a
ranks <saito@room.rat.cis.k.hosei.ac.jp>
2017-11-11 17:27:52 +0900

    typo

--- src/model/row/user.go
+++ src/model/row/user.go
@@ -8,8 +8,8 @@ type WantedlyUser struct {
 	Id        int       `json:"id" xorm:"not null pk autoincr INT(10)"`
 	Name      string    `json:"name" xorm:"not null VARCHAR(255)"`
 	Email     string    `json:"email" xorm:"not null VARCHAR(255)"`
-	CreatedAt time.Time `json:"created_at" xorm:"'created_at'"`
-	UpdatedAr time.Time `json:"updated_ar" xorm:"'updated_at'"`
+	CreatedAt time.Time `json:"created_at" xorm:"<-"`
+	UpdatedAt time.Time `json:"updated_at" xorm:"<-"`
 }
 
 func (WantedlyUser) TableName() string {
