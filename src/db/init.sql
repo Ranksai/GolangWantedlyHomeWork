e25d5e6d8b46a09e6d2ae7a47adf94183929d239
ranks <saito@room.rat.cis.k.hosei.ac.jp>
2017-11-11 14:24:50 +0900

    "テーブルの制約変更"

--- src/db/init.sql
+++ src/db/init.sql
@@ -1,9 +1,10 @@
 CREATE TABLE wantedlyusers (
- id int primary key AUTO_INCREMENT,
+ id serial,
  name varchar(255) not null,
- email varchar(255) not null,
- created_at timestamp not null default current_timestamp,
- updated_at timestamp not null default current_timestamp
+ email varchar(255) not null unique,
+ created_at timestamp default current_timestamp,
+ updated_at timestamp default current_timestamp,
+ primary key(id)
 );
 
 create function set_update_time() returns opaque as '
